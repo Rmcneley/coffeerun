b3bcbec5a10985dc3b99984535f4c16e72962dac
Richard McNeley <rmcneley@csu.fullerton.edu>
2017-09-25 19:47:17 -0700

    made window = global

--- tests/main.js
+++ tests/main.js
@@ -1,14 +1,17 @@
-eval(require('fs').readFileSync('tests/datastore.js', 'utf8'));
-eval(require('fs').readFileSync('tests/truck.js', 'utf8'));
+var window = global;
+eval(require('fs').readFileSync('scripts/datastore.js', 'utf8'));
+eval(require('fs').readFileSync('scripts/truck.js', 'utf8'));
 
-(function(global) {
+
+(function(window) {
   'use strict';
-  var App = global.App;
+
+  var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var myTruck = new Truck('MC1', new DataStore());
-  global.myTruck = myTruck;
-})(global);
+  window.myTruck = myTruck;
+})(window);
 
 eval(require('fs').readFileSync('tests/test_datastore.js', 'utf8'));
 eval(require('fs').readFileSync('tests/test_truck.js', 'utf8'));
